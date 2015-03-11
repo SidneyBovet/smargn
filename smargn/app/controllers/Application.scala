4fd9d294ed7d8b98feba78e78be0f97c6bdd74b2
Valentin Rutz <valentin.rutz@epfl.ch>
2015-03-11 10:28:21 +0100

    Implements Test stuff to show how it works

--- smargn/app/controllers/Application.scala
+++ smargn/app/controllers/Application.scala
@@ -1,12 +1,23 @@
 package controllers
 
+import ngrams.Test
 import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("Hello world"))
+  }
+
+  def test = Action {
+    val v = new Test("I <3 Ngrams")
+    Ok(views.html.test(v.getMsg))
+  }
+
+  def defaultTest = Action {
+    val v = new Test("dont care")
+    Ok(views.html.test(v.getDefaultMsg))
   }
 
 }
---
+++ smargn/app/ngrams/Test.scala
@@ -0,0 +1,9 @@
+package ngrams
+
+/**
+ * Created by Valentin on 11/03/15.
+ */
+class Test(msg: String) {
+  def getMsg = msg
+  def getDefaultMsg = "Hello World"
+}
