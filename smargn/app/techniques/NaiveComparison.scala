bad7edbf8544177b71cd16b320ad8b792e5527e6
Joanna Salathe <joanna.salathe@epfl.ch>
2015-03-26 17:56:28 +0100

    Finish the code for NaiveComparison (difference and division + scale method max and average) and integration in Spark for displaying matching word (latter also graph)

--- smargn/app/techniques/NaiveComparison.scala
+++ smargn/app/techniques/NaiveComparison.scala
@@ -5,57 +5,51 @@ import org.apache.spark.{SparkContext, SparkConf}
 import java.io.{File, PrintWriter}
 
 /**
+ *
  * Created by Valentin on 17/03/15.
+ * Updated by Joanna (main code) on 26/03/15.
  */
 object NaiveComparison {
-  def run(word:String, inputDir: String, outputFile: String) = {
+  def run(word: String, inputDir: String, outputFile: String) = {
     val spark = Spark.ctx
 
     val data = spark.textFile(inputDir)
 
 
-    //difference value that is accepted to consider two array values similar
-    val acceptedDifference = 3;
-
     //parse data to have list of value
-    val parsedData = data.map(line => line.split(" "))
     //group data in the format (word, Array(list of occurrence))
-    val wordOccurrencesData = parsedData.map(i => (i.head, i.tail.toList))
-    // Get occurrences of the word
-    val wordOccurrencesTuple = wordOccurrencesData.filter {
-      case (w, o) => w == word
+    val formattedData = data.map(line => line.split(" ")).map((i: Array[String]) => (i.head, i.tail.map(y => y.toDouble)))
+
+
+    //difference value that is accepted to consider two array values similar
+    val acceptedDifferenceNaiveDifference = 3
+    val acceptedDifferenceNaiveDifferenceMaxScale = 0.2
+    val acceptedDifferenceNaiveDifferenceAvgScale = 0.2
+    val acceptedDifferenceNaiveDivision = 0.5
+
+
+    //TODO deal with word not find in data
+    //get word that we want to have its similar words from run's argument and find it in the data
+    val testedWord = formattedData.filter { case (w, o) => w == word
     }.first
-    val wordOccurrences = wordOccurrencesTuple._2
-    val similarities = wordOccurrencesData.filter(e => e._1 != word && (e._2.map(_.toInt) zip wordOccurrences.map(_.toInt)).:\(true){
-      case ((x, y), acc) => acc && math.abs(x - y) < acceptedDifference
-    })
-    similarities.map(_._1).saveAsTextFile(outputFile)
 
-    val similaritiesLocal = similarities.collect.toList
+    //TODO find a way to choose function
+    //val similarWordsDifference = naiveDifference(formattedData, testedWord, acceptedDifferenceNaiveDifference)
+    //val similarWordsDifferenceMax = naiveDifference(formattedData.map(proportionalScalarMax(_)), proportionalScalarMax(testedWord), acceptedDifferenceNaiveDifferenceMaxScale)
+    //val similarWordsDifferenceAverage = naiveDifference(formattedData.map(proportionalScalarAverage(_)), proportionalScalarAverage(testedWord), acceptedDifferenceNaiveDifferenceAvgScale)
+    val similarWordsDivision = naiveDivision(formattedData, testedWord, acceptedDifferenceNaiveDivision)
+    //similarWordsDivision.map(_._1).saveAsTextFile(outputFile)
+    val similaritiesLocal = similarWordsDivision.collect().toList
+    val wordOccurrences = testedWord._2
 
 
-//    //add the wordOccurrences to the arrays for future comparison
-//    val zipDataTestedWord = wordOccurrencesData.map(x => (x._1, wordOccurrences.zip(x._2)))
-//    //test similarity criteria between each data word array and the tested word
-//    val booleanDataTestedWord = zipDataTestedWord.map(x => (x._1, x._2.map(x => (x._1.toInt, x._2.toInt)).map(x => math.abs((x._1 - x._2)) < acceptedDifference)))
-//    //filter the arrays that have at least one value that didn't pass the similarity test
-//    val filterSimilarity = booleanDataTestedWord.map(x => (x._1, x._2.filter(!_))).filter(_._2.length == 0)
-//    val similarWords = filterSimilarity.map(x => x._1)
-//
-//    val completeResult: List[(String, Array[Int])] = filterSimilarity.collect.toList
-//    val res = completeResult.map(_._1)
-//
-//    similarWords.saveAsTextFile(outputFile)
-
-    val firstLine = wordOccurrences.:\("word") {
-      case (_, acc) => acc + "," + YearCnt.getNextYear
+    //TODO finish display graph
+    val firstLine = wordOccurrences.:\("word") { case (_, acc) => acc + "," + YearCnt.getNextYear
     }
-    val toPrint = firstLine :: (wordOccurrencesTuple :: similaritiesLocal).map {
-      case (w, l) => w + l.:\(""){ case (o, acc) => acc + "," + o }
+    val toPrint = firstLine :: (testedWord :: similaritiesLocal).map { case (w, l) => w + l.:\("") { case (o, acc) => acc + "," + o}
     }
-    printToFile(new File(outputFile + "data.csv")) {
-      p => toPrint.foreach(p.println)
-    }
+    //printToFile(new File(outputFile + "data.csv")) { p => toPrint.foreach(p.println)
+    //}
     similaritiesLocal.map(_._1)
   }
 
@@ -67,10 +61,87 @@ object NaiveComparison {
       p.close()
     }
   }
+
+  /**
+   * Find the min and max of an array
+   * @param array array we want to find its min and max
+   * @return tuple of min and max of the array
+   */
+  //taken from http://stackoverflow.com/questions/20285209/find-min-and-max-elements-of-array
+  def findMinAndMax(array: Array[Double]): (Double, Double) = {
+    // a non-empty array
+    if (array.length != 0) {
+      val initial = (array.head, array.head) // a tuple representing min-max
+      array.foldLeft(initial) { (acc, x) =>
+        if (x < acc._1) {
+          (x, acc._2)
+        } else if (x > acc._2) {
+          (acc._1, x)
+        } else {
+          acc
+        }
+      }
+    } else {
+      (-1.0, -1.0)
+    }
+  }
+
+  /**
+   * Scale the word, frequency tuple with the help of its maximum frequency
+   * @param wordFreq tuple of word and its frequency
+   * @return tuple of word and its max-scaled frequency
+   */
+  def proportionalScalarMax(wordFreq: (String, Array[Double])): (String, Array[Double]) = {
+    val max = findMinAndMax(wordFreq._2)._2
+    (wordFreq._1, wordFreq._2.map(x => x / max))
+  }
+
+  /**
+   * Scale the word, frequency tuple with the help of its average frequency
+   * @param formattedWord tuple of word and its frequency
+   * @return tuple of word and its average-scaled frequency
+   */
+  def proportionalScalarAverage(formattedWord: (String, Array[Double])): (String, Array[Double]) = {
+    val average = formattedWord._2.reduceLeft(_ + _)
+    (formattedWord._1, formattedWord._2.map(x => x / average))
+  }
+
+  /**
+   * Compare a word,frequency tuple with a collection of word, frequency tuples to find similar words by computing array's elements difference
+   * @param data collection of word, frequency to tuple to look into
+   * @param testedWord word that we want to find its similar word
+   * @param acceptedDifference difference between two array value that we accept
+   * @return words that are similar
+   */
+  def naiveDifference(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), acceptedDifference: Double): RDD[(String, Array[Double])] = {
+    //add the testedWord values to the arrays and compute difference for future comparison
+    val zipDataTestedWord = data.map(x => (x._1, (testedWord._2).zip(x._2).map(x => math.abs(x._1 - x._2)), x._2))
+    //test similarity criteria between each data word array and the tested word
+    val booleanDataTestedWord = zipDataTestedWord.map(x => (x._1, x._2.map(y => y <= acceptedDifference), x._3))
+    //filter the arrays that have at least one value that didn't pass the similarity test
+    booleanDataTestedWord.map(x => (x._1, x._2.filter(_ == false), x._3)).filter(x => x._2.length == 0 && x._1 != testedWord._1).map(x => (x._1, x._3))
+  }
+
+  /**
+   * Compare the ratio of word's frequency with the collection word's frequency to find similar words by computing the ratio line
+   * @param data collection of word, frequency to tuple to look into
+   * @param testedWord word that we want to find its similar word
+   * @param acceptedDifference straightness of the curve that we accept
+   * @return words that are similar
+   */
+  def naiveDivision(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), acceptedDifference: Double): RDD[(String, Array[Double])] = {
+    //add the testedWord values to the arrays and compute division for future comparison
+    val dividedDataTestedWord = data.map(x => (x._1, testedWord._2.zip(x._2).map(x => x._1 / x._2), x._2))
+    //could be useful for testing purpose
+    //val tempDifferenceMaxMin = dividedDataTestedWord.map(x => (x._1, (((findMinAndMax(x._2))._2)- ((findMinAndMax(x._2))._1))))
+    val booleanDataTestedWord = dividedDataTestedWord.map(x => (x._1, ((findMinAndMax(x._2)._2 - findMinAndMax(x._2)._1) < acceptedDifference), x._3))
+    booleanDataTestedWord.filter(x => x._2 == true && x._1 != testedWord._1).map(x => (x._1, x._3))
+  }
 }
 
 object YearCnt {
   var year = 2000
+
   def getNextYear = {
     year = year + 1
     year
