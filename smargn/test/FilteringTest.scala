8aad252eac6a2d03f532baa04ddf02152613814b
Sidney Bovet <sidney.bovet@gmail.com>
2015-04-11 20:02:59 +0200

    Adds some testing for the filtering and utils methods

--- smargn/test/FilteringTest.scala
+++ smargn/test/FilteringTest.scala
@@ -1,6 +1,21 @@
+import org.scalatest._
+
 /**
  * Created with love by sidney.
  */
-class FilteringTest {
+class FilteringTest extends SparkTestUtils with ShouldMatchers {
 
+  /* sample spark test to be used for a broader testing of the filtering
+  sparkTest("spark filter test") {
+    val data = sc.parallelize(1 to 1e6.toInt)
+    data.filter {
+      _ % 2 == 0
+    }.count should be(5e5.toInt)
+  }
+  */
+
+  test("mean test") {
+    val a = Array(0, 1, 2, 3, 4)
+    utils.Filtering.mean(a) should be(2)
+  }
 }
