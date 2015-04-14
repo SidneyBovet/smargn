5985cdd21536fc3deef7897c58ab981dd7a1e10a
Sidney Bovet <sidney.bovet@gmail.com>
2015-04-14 23:50:30 +0200

    Permissions change and minor changes and refinements to the script that runs the whole data generation pipeline

--- data-generation/run-all.sh
+++ data-generation/run-all.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-#TODO: remove all output folders
+echo "Starting the whole data generation pipeline..."
 
 ## parsing ##
 cd parsing
 mvn package &&
-hadoop fs -rm -r /projects/temporal-profiles/data-generation/parsing &&
+hadoop fs -rm -r /projects/temporal-profiles/data-generation/parsing ;
 hadoop jar target/parsing-0.0.1-SNAPSHOT.jar ch.epfl.bigdata15.ngrams.parsing.ParseDriver /projects/temporal-profiles/data-generation/input /projects/temporal-profiles/data-generation/parsing &&
 cd .. &&
 
@@ -16,12 +16,20 @@ hadoop fs -rm -r /projects/temporal-profiles/data-generation/1gram-generation/st
 hadoop jar target/1gram-step1-0.0.1-SNAPSHOT.jar mapred.MapReduce /projects/temporal-profiles/data-generation/parsing /projects/temporal-profiles/data-generation/1gram-generation/step1 &&
 cd ../step2 &&
 mvn package &&
-hadoop fs -rm -r /projects/temporal-profiles/data-generation/1gram-generation/step2 &&
+hadoop fs -rm -r /projects/temporal-profiles/data-generation/1gram-generation/step2 ;
 hadoop jar target/1gram-step2-0.0.1-SNAPSHOT.jar mapred.MapReduce2 /projects/temporal-profiles/data-generation/1gram-generation/step1 /projects/temporal-profiles/data-generation/1gram-generation/step2 &&
 cd ../.. &&
 
 ## 1gram-cleaning ##
 cd 1gram-cleaning &&
 sbt package &&
-hadoop fs -rm /projects/temporal-profiles/data-generation/clean-1gram &&
-spark-submit --class OneGramCleaning --master yarn-client --num-executor 20 target/scala-2.10/onegramcleaning_2.10-1.0.jar "hdfs:///projects/temporal-profiles/data-generation/1gram-generation/step2" "hdfs:///projects/temporal-profiles/data-generation/clean-1gram" 
+hadoop fs -rm /projects/temporal-profiles/data-generation/clean-1gram ;
+spark-submit --class OneGramCleaning --master yarn-client --num-executors 20 target/scala-2.10/onegramcleaning_2.10-1.0.jar "hdfs:///projects/temporal-profiles/data-generation/1gram-generation/step2" "hdfs:///projects/temporal-profiles/data-generation/clean-1gram" &&
+echo "------------------------------" &&
+echo "Pipeline successfully achieved" &&
+echo "------------------------------"
+
+echo -n "Attempting to change rights on the output folder..."
+hadoop fs -chgrp -R bigdata2015-temporalprofiles /projects/temporal-profiles/data-generation/ &&
+hadoop fs -chmod -R g+w /projects/temporal-profiles/data-generation/ &&
+echo " done."
