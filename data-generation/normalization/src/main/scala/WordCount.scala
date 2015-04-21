3072015a50b35893ce889b3b753427dd5a653d44
Sidney Bovet <sidney.bovet@gmail.com>
2015-04-21 16:50:02 +0200

    Data statistics now feasible before AND after the cleaning.

--- data-generation/normalization/src/main/scala/WordCount.scala
+++ data-generation/normalization/src/main/scala/WordCount.scala
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.fs.FileSystem
+import org.apache.spark.rdd.RDD
 import java.io.PrintWriter
 
 object WordCountPerYear {
@@ -16,25 +17,24 @@ object WordCountPerYear {
 
     val spark = new SparkContext(new SparkConf().setAppName("WordCountPerYear"))
 
-    // open the files
-    val files = spark.textFile(args(0))
-
-    val counts = files.map(line => {
-      val words = line.split(" ")
-      val year = words(0).toInt
-      (year,words.length - 1)
-    })
-    val finalArray = counts.sortBy(_._1).map(_._2).collect
+    // open the file
+    val file = dataFormatter(spark.textFile(args(0)))
+    val acc = Array.fill[Int](file.take(1)(0).length)(0)
+    val sums = file.fold(acc)((a,e) => a.zip(e).map(el=>el._1+el._2))
 
     // Write cleaned 1grams on HDFS
     val conf = new Configuration()
     val fs = FileSystem.get(conf)
     val out = fs.create(new Path(args(1)))
-    finalArray.foreach(el => out.write((el.toString+",").getBytes))
+    sums.foreach(el => out.write((el.toString+",").getBytes))
     out.close()
     fs.close()
 
     spark.stop()
+  }
+
+  def dataFormatter(data: RDD[(String)]): RDD[Array[Int]] = {
+    return data.map(line => line.split(" ")).map((i: Array[String]) => i.tail.map(y => y.toInt))
   }
 }
 
