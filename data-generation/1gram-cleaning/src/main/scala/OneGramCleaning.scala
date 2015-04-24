9754a8c5cfad2f4862297e69eb0d4f40a9b194e0
Sidney Bovet <sidney.bovet@gmail.com>
2015-04-24 19:53:06 +0200

    Data cleaning now outputs non human readable file of normalized words

--- data-generation/1gram-cleaning/src/main/scala/OneGramCleaning.scala
+++ data-generation/1gram-cleaning/src/main/scala/OneGramCleaning.scala
@@ -27,24 +27,26 @@ object OneGramCleaning {
     val filteredLines = parsedLines.filter(_._2.sum > minOcc)
 
     // sum over each year
-    val acc = Array.fill[Int](filteredLines.take(1)(0)._2.length)(0)
+    val yearCount = filteredLines.take(1)(0)._2.length
+    val acc = Array.fill[Int](yearCount)(0)
     val sumsPerYeas = filteredLines.map(_._2).fold(acc)((a,e) => a.zip(e).map(el=>el._1+el._2))
 
-    val finalArray = Array(("SumPerYear",sumsPerYeas))++filteredLines.collect
+    val normalizedLines = filteredLines.map(el =>
+      (el._1, el._2.zip(sumsPerYeas).map(el => el._1.toDouble/el._2.toDouble)))
 
     // Write cleaned 1grams on HDFS
     val conf = new Configuration()
     val fs = FileSystem.get(conf)
     val out = fs.create(new Path(args(1)))
-    printArray(finalArray,out)
+    printArray(yearCount,normalizedLines.collect,out)
     out.close()
 
     //Compute and write samples
     if(args.length >= 4) {
       val sampleList = sc.textFile(args(2)).flatMap(_.split("\\s+")).collect
-      val samples = filteredLines.filter(x => sampleList.contains(x._1))
+      val samples = normalizedLines.filter(x => sampleList.contains(x._1))
       val out = fs.create(new Path(args(3)))
-      printArray(samples.collect,out)
+      printArray(yearCount,samples.collect,out)
       out.close()
     }
 
@@ -52,11 +54,13 @@ object OneGramCleaning {
     sc.stop()
   }
 
-  def printArray(a:Array[(String,Array[Int])], out:FSDataOutputStream) = {
+  def printArray(numYears:Int,a:Array[(String,Array[Double])], out:FSDataOutputStream) = {
+    out.writeInt(numYears)
     a.foreach(el => {
-      out.write(el._1.getBytes("UTF-8"))
-      el._2.foreach(int => out.write((" "+int.toString).getBytes("UTF-8")))
-      out.write("\n".getBytes("UTF-8"))
-    })
+      val wordBytes = el._1.getBytes("UTF-8")
+      out.writeInt(wordBytes.length)
+      out.write(wordBytes)
+      el._2.foreach(freq => out.writeDouble((freq)))
+      })
   }
 }
