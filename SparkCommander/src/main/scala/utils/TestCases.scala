a5b91bd20359218d7f308b0dde7c6895c9c2906d
Fabien Jolidon <fabien@linuxserver01.linuxserver>
2015-04-29 14:46:15 +0200

    moved TestCases to the SparkCommander project

---
+++ SparkCommander/src/main/scala/utils/TestCases.scala
@@ -0,0 +1,41 @@
+package utils
+
+import org.apache.spark.rdd.RDD
+import utils.Launcher.Technique
+
+/**
+ * Created by fabien on 4/28/15.
+ */
+object TestCases {
+
+
+  def count(result : RDD[(String)],  wordList: List[String]) : Int = {
+    var value = 0
+    wordList.foreach(
+      word => result.foreach(
+        x => if (x == word) {
+          value += 1
+        }
+      )
+    )
+    value
+  }
+
+  def test(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), similarWords: List[String],
+           differentWords: List[String], parameters: List[Double],
+           similarityTechnique: Technique): Double = {
+
+    var result : RDD[(String)] = similarityTechnique(data,testedWord,parameters)
+
+
+    var simWords = count(result,similarWords)
+    var diffWords = count(result,differentWords)
+    var simRatio = simWords.toDouble / similarWords.size.toDouble
+    var diffRatio = diffWords.toDouble / similarWords.size.toDouble
+
+
+
+
+    (simRatio + (1-diffRatio))/2
+  }
+}
