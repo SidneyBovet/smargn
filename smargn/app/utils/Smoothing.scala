16d242fd46090891cd411d72db574c68cae0b9c4
Merok <john.gaspoz@epfl.ch>
2015-04-29 15:46:40 +0200

    add smoothing function

--- smargn/app/utils/Smoothing.scala
+++ smargn/app/utils/Smoothing.scala
@@ -11,41 +11,20 @@ object Smoothing {
 
 
   def averageByWindow(arr: Array[Double], window: Double): Array[Double] = {
-    var smoothed: Array[Double] = Array(arr.head)
-    var from: Int = 0
-    var to: Int = window.toInt
-    var i = 0
-
-    for (i <- 0 to (arr.length - 1)) {
-
-      val smoothPoint: Double = arr.slice(from, to).sum / arr.slice(from, to).length
-      smoothed = smoothed :+ smoothPoint
-      from = from + 1
-      to = to + 1
-    }
-    smoothed
+    val chunks = arr.grouped(window.toInt).toArray
+    return chunks.map(x => x.sum / x.size)
   }
 
   /**
    * Given a parameter x, smooth the curve by doing the average of x points together.
    * @param data collection of (word, frequency)
-   * @param smoothingValue the number of points to average together if window = x : x points will be averaged together
+   * @param parameters the number of points to average together
    * @return the smoothed data
    */
-  def smooth(data: RDD[(String, Array[Double])], smoothingValue: Double): RDD[(String, Array[Double])] = {
+  def smooth(data: RDD[(String, Array[Double])], parameters: List[Double]): RDD[(String, Array[Double])] = {
+    val smoothingValue = parameters.head
     val smoothedData = data.map(x => (x._1, averageByWindow(x._2, smoothingValue)))
-    smoothedData
-  }
-
-  /**
-   * Given a parameter x, smooth the curve by doing the average of x points together.
-   * @param data collection of (word, frequency)
-   * @param smoothingValue the number of points to average together if window = x : x points will be averaged together
-   * @return the smoothed data
-   */
-  def smooth(data: (String, Array[Double]), smoothingValue: Double): (String, Array[Double]) = {
-    val smoothedData = (data._1, averageByWindow(data._2, smoothingValue))
-    smoothedData
+    return smoothedData
   }
 
 }
