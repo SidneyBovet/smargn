71be6e67addf89e21c1f33ff7af415d922afbe18
Merok <john.gaspoz@epfl.ch>
2015-04-29 15:07:26 +0200

    fix error in inverse function. Implement inverseDivergent Function

--- SparkCommander/src/main/scala/techniques/NaiveShiftComparison.scala
+++ SparkCommander/src/main/scala/techniques/NaiveShiftComparison.scala
@@ -3,28 +3,37 @@ package techniques
 import org.apache.spark.rdd.RDD
 
 /**
- * Created by johngaspoz and Fabien on 12.04.15
+ * Created by johngaspoz on 19.04.15
  * Naive functions that compute the similar shifted words list of a given word based on comparison techniques
  */
-object NaiveShiftComparison {
+object Divergence {
 
   import utils.SubTechniques._
 
   /**
-   * NaiveDifference shifted 3 times
    * @param data collection of word, frequency to tuple to look into
    * @param testedWord word that we want to find its similar word
    * @param parameters L(0) contains the accepted difference between two array value that we accept
    *
-   * @return words that are similar but shifted between 0 and 3 times (right or left)
+   * @return words that are at first similar and then diverge
    */
-  def naiveDifferenceShift(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]),
-                           parameters: List[Double]): RDD[(String)] = {
+  def naiveDifferenceDivergence(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]),
+                                parameters: List[Double]): RDD[(String)] = {
 
-    //TODO
-    //when we have a parameters list of Any, we could have 2 distinct lists of parameters, one for the technique
-    //and one for the parameters of the shift itself (len and step)
-    shift(data, testedWord, parameters, NaiveComparisons.naiveDifferenceScalingMax, 3, 1)
+    divergence(data, testedWord, parameters, NaiveComparisons.naiveDifferenceScalingMax)
+  }
+
+  /**
+   * @param data collection of word, frequency to tuple to look into
+   * @param testedWord word that we want to find its similar word
+   * @param parameters L(0) contains the accepted difference between two array value that we accept
+   *
+   * @return words that are at divergent and then are similar
+   */
+  def naiveInverseDifferenceDivergence(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]),
+                                       parameters: List[Double]): RDD[(String)] = {
+    val dataInversed = data.map(x => (x._1, x._2.reverse))
+    divergence(dataInversed, (testedWord._1, testedWord._2.reverse), parameters, NaiveComparisons.naiveDifferenceScalingMax)
   }
 
 }
