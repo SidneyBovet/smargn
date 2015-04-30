465ac6c3f1f9c8a5606e540a2a73a02504d3fd2d
Joanna <joanna.salathe@epfl.ch>
2015-04-30 16:26:17 +0200

    Correct the test case @MathieuMonney mentioned and cleaned the code

--- smargn/app/techniques/NaiveComparisons.scala
+++ smargn/app/techniques/NaiveComparisons.scala
@@ -1,6 +1,5 @@
 package techniques
 
-import controllers.Spark
 import org.apache.spark.rdd.RDD
 import utils.Scaling._
 import utils.SubTechniques._
@@ -13,6 +12,34 @@ import utils.TopK._
 object NaiveComparisons {
 
   import utils.ComputationUtilities._
+
+  //TOPK version
+
+  def naiveDifferenceTopK(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), parameters: List[Double]): RDD[(String)] = {
+    val k = parameters.head
+    val acceptedDifference = parameters(1)
+    val order = (x: (String, Double), y: (String, Double)) => if (x._2 != y._2) {
+      x._2 < y._2
+    } else x._1 < y._1
+    val retrievedWords = retrieveTopK(k.toInt, naiveDifferenceMetricTopK, data, testedWord, order, List(acceptedDifference))
+    data.sparkContext.parallelize(retrievedWords)
+  }
+
+  def naiveDifferenceTopKScalingMax(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), parameters: List[Double]): RDD[(String)] = {
+    naiveDifferenceTopK(data.map(proportionalScalarMax), proportionalScalarMax(testedWord), parameters)
+  }
+
+  def naiveDivisionTopK(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), parameters: List[Double]): RDD[(String)] = {
+    val k = parameters.head
+    val order = (x: (String, Double), y: (String, Double)) => if (x._2 != y._2) {
+      x._2 < y._2
+    } else x._1 < y._1
+    val retrievedWords = retrieveTopK(k.toInt, naiveDivisionMetricTopK, data, testedWord, order)
+    data.sparkContext.parallelize(retrievedWords)
+  }
+
+
+  //older version
 
   /**
    * Compare a word,frequency tuple with a collection of word, frequency tuples to find similar words by computing
@@ -45,28 +72,8 @@ object NaiveComparisons {
     data.map(x => (testedWord, x)).map(y => (y._2._1, naiveDivisionMetric(y._1, y._2, List(acceptedDifference)))).filter(y => y._2 != Double.MaxValue && y._1 != testedWord._1).map(_._1)
   }
 
-  def naiveDifferenceTopK(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), parameters: List[Double]): RDD[(String)] = {
-    val k = parameters.head
-    val acceptedDifference = parameters(1)
-    val order = (x: (String, Double), y: (String, Double)) => if (x._2 != y._2) {
-      x._2 < y._2
-    } else x._1 < y._1
-    val retrievedWords = retrieveTopK(k.toInt, naiveDifferenceMetricTopK, data, testedWord, order, List(acceptedDifference))
-    data.sparkContext.parallelize(retrievedWords)
-  }
 
-  def naiveDifferenceTopKScalingMax(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), parameters: List[Double]): RDD[(String)] = {
-    naiveDifferenceTopK(data.map(proportionalScalarMax), proportionalScalarMax(testedWord), parameters)
-  }
-
-  def naiveDivisionTopK(data: RDD[(String, Array[Double])], testedWord: (String, Array[Double]), parameters: List[Double]): RDD[(String)] = {
-    val k = parameters.head
-    val order = (x: (String, Double), y: (String, Double)) => if (x._2 != y._2) {
-      x._2 < y._2
-    } else x._1 < y._1
-    val retrievedWords = retrieveTopK(k.toInt, naiveDivisionMetricTopK, data, testedWord, order)
-    data.sparkContext.parallelize(retrievedWords)
-  }
+  //scaling version
 
   /**
    * Apply the max scaling function before calling the NaiveDifference similarity function
@@ -165,10 +172,63 @@ object NaiveComparisons {
   /** *******************************************************************************************************
     * Metrics
     * ******************************************************************************************************* */
+
+
+  //NEW ONES
+
+  /**
+   *
+   * @param word1 first temporal profile
+   * @param word2 second temporal profile
+   * @param parameters L(0) contains the accepted difference between two array value that we accept,
+   * @return sum of differences of each element divided by size of the resulting array (filtered a priori by the accepted difference if too extreme value)
+   */
+  def naiveDifferenceMetricTopK(word1: (String, Array[Double]), word2: (String, Array[Double]), parameters: List[Double] = List(15)): Double = {
+    val acceptedDifference = parameters.head
+    val zipped = word1._2.zip(word2._2)
+    val zippedDif = zipped.map(x => math.abs(x._1 - x._2))
+    val trueDif = zippedDif.map(_ <= acceptedDifference).filter(_ == true)
+    if (trueDif.length > 0) {
+      zippedDif.sum / trueDif.length
+    } else {
+      zippedDif.sum
+    }
+  }
+
+
+  /**
+   *
+   * @param word1 first temporal profile
+   * @param word2 second temporal profile
+   * @return difference of min element and max element of the "line" (i.e. w1/w2 element)
+   */
+  def naiveDivisionMetricTopK(word1: (String, Array[Double]), word2: (String, Array[Double]), parameters: List[Double] = List()): Double = {
+    val zipped = word1._2.zip(word2._2)
+    val divided = zipped.map(x => math.abs((if (x._1 == 0) {
+      if (x._2 == 0) {
+        1
+      } else {
+        x._2 * x._2
+      }
+    }
+    else {
+      x._1
+    }) / (if (x._2 == 0) {
+      1
+    } else {
+      x._2
+    })))
+    val minMax = findMinAndMax(divided)
+    minMax._2 - minMax._1
+  }
+
+  //OLDER ONES
+
+
   /**
    * Compute the metric of similarity based on a naiveDifference approach for two words.
-   * @param word1
-   * @param word2
+   * @param word1 first temporal profile
+   * @param word2 second temporal profile
    * @param parameters L(0) contains the accepted difference between two array value that we accept and L(1) the percentage of non-similar values that we accept
    * @return sum of differences of each element if words are considered similar, Double.MaxValue otherwise
    */
@@ -180,7 +240,7 @@ object NaiveComparisons {
     }
     val zipped = word1._2.zip(word2._2)
     val zippedDif = zipped.map(x => math.abs(x._1 - x._2))
-    if (zippedDif.map(_ <= acceptedDifference).count(_ == false) <= zippedDif.size * acceptedFalse) {
+    if (zippedDif.map(_ <= acceptedDifference).count(_ == false) <= zippedDif.length * acceptedFalse) {
       zippedDif.sum
     } else {
       Double.MaxValue
@@ -190,8 +250,8 @@ object NaiveComparisons {
   //problem metric only base on 2 points ==> very biased
   /**
    * Compute the metric of similarity based on a naiveDivision approach for two words.
-   * @param word1
-   * @param word2
+   * @param word1 first temporal profile
+   * @param word2 second temporal profile
    * @param parameters L(0) contains the accepted difference between min and max value of the created line
    * @return difference of min element and max element of the "line" (i.e. w1/w2 element) if words are considered similar, Double.MaxValue otherwise
    */
@@ -218,38 +278,6 @@ object NaiveComparisons {
     } else {
       Double.MaxValue
     }
-  }
-
-  def naiveDifferenceMetricTopK(word1: (String, Array[Double]), word2: (String, Array[Double]), parameters: List[Double] = List(15)): Double = {
-    val acceptedDifference = parameters.head
-    val zipped = word1._2.zip(word2._2)
-    val zippedDif = zipped.map(x => math.abs(x._1 - x._2))
-    val trueDif = zippedDif.map(_ <= acceptedDifference).filter(_ == true)
-    if (trueDif.length > 0) {
-      zippedDif.sum / trueDif.length
-    } else {
-      zippedDif.sum
-    }
-  }
-
-  def naiveDivisionMetricTopK(word1: (String, Array[Double]), word2: (String, Array[Double]), parameters: List[Double] = List()): Double = {
-    val zipped = word1._2.zip(word2._2)
-    val divided = zipped.map(x => math.abs((if (x._1 == 0) {
-      if (x._2 == 0) {
-        1
-      } else {
-        x._2 * x._2
-      }
-    }
-    else {
-      x._1
-    }) / (if (x._2 == 0) {
-      1
-    } else {
-      x._2
-    })))
-    val minMax = findMinAndMax(divided)
-    minMax._2 - minMax._1
   }
 
 
